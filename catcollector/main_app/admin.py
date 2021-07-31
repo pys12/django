e38190ca631d026edcf583bc78e3f21302e25600
pys12 <peiyishi11@gmail.com>
2021-07-30 23:53:23 -0400

    added toy

--- catcollector/main_app/admin.py
+++ catcollector/main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cat, Feeding
+from .models import Cat, Feeding,Toy
 from .models import Owner
 from .models import Adoption
 # Register your models here
@@ -7,3 +7,5 @@ admin.site.register(Cat)
 admin.site.register(Owner)
 admin.site.register(Adoption)
 admin.site.register(Feeding)
+admin.site.register(Toy)
+
--- catcollector/main_app/views.py
+++ catcollector/main_app/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render,redirect
-from .models import Cat
+from .models import Cat,Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .forms import FeedingForm
-# Define the home view
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+  success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['name','breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
+
 def home(request):
   return render(request,'home.html')
 
@@ -17,11 +31,12 @@ def cats_index(request):
 
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
-  # instantiate FeedingForm to be rendered in the template
+  toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
   feeding_form = FeedingForm()
   return render(request, 'cats/detail.html', {
     # include the cat and feeding_form in the context
-    'cat': cat, 'feeding_form': feeding_form
+    'cat': cat, 'feeding_form': feeding_form,
+    'toys': toys_cat_doesnt_have
   })
 
 def add_feeding(request, cat_id):
@@ -35,15 +50,20 @@ def add_feeding(request, cat_id):
     new_feeding.save()
   return redirect('detail', cat_id=cat_id)
 
-class CatCreate(CreateView):
-  model = Cat
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
   fields = '__all__'
-  success_url = '/cats/'
 
-class CatUpdate(UpdateView):
-  model = Cat
-  fields = ['breed', 'description', 'age']
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
 
-class CatDelete(DeleteView):
-  model = Cat
-  success_url = '/cats/'
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
